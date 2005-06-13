01ec3a11c0325307488188b016fdf5f626339a60
Jonathon Fowler <jf@jonof.id.au>
2005-06-13 16:15:23 +0000

    A couple of minor bits in anticipation for Mac porting.

--- include/compat.h
+++ include/compat.h
@@ -10,113 +10,177 @@
 #include <stdarg.h>
 
 #ifdef __compat_h_macrodef__
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <time.h>
-#include <ctype.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#if defined(LINUX) || defined(BSD)
-#include <unistd.h>
-#else
-#include <io.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <time.h>
+# include <ctype.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# if defined(LINUX) || defined(BSD)
+#  include <unistd.h>
+# else
+#  include <io.h>
+# endif
 #endif
+
+#if defined(__WATCOMC__)
+# define inline __inline
+# define int64 __int64
+# define longlong(x) x##i64
+#elif defined(_MSC_VER)
+# define inline __inline
+# define int64 __int64
+# define longlong(x) x##i64
+#else
+# define longlong(x) x##ll
+typedef long long int64;
+#endif
+
+#ifndef NULL
+# define NULL ((void *)0)
+#endif
+
+#if defined(LINUX)
+# include <endian.h>
+# if __BYTE_ORDER == __LITTLE_ENDIAN
+#  define B_LITTLE_ENDIAN 1
+#  define B_BIG_ENDIAN    0
+# elif __BYTE_ORDER == __BIG_ENDIAN
+#  define B_LITTLE_ENDIAN 0
+#  define B_BIG_ENDIAN    1
+# endif
+# include <linux/byteorder/swab.h>
+# define B_SWAP64(x) __swab64(x)
+# define B_SWAP32(x) __swab32(x)
+# define B_SWAP16(x) __swab16(x)
+#elif defined(BSD)
+# include <sys/endian.h>
+# if _BYTE_ORDER == _LITTLE_ENDIAN
+#  define B_LITTLE_ENDIAN 1
+#  define B_BIG_ENDIAN    0
+# elif _BYTE_ORDER == _BIG_ENDIAN
+#  define B_LITTLE_ENDIAN 0
+#  define B_BIG_ENDIAN    1
+# endif
+# define B_SWAP64(x) __bswap64(x)
+# define B_SWAP32(x) __bswap32(x)
+# define B_SWAP16(x) __bswap16(x)
+#elif defined(DARWIN)
+# if defined(__LITTLE_ENDIAN__)
+#  define B_LITTLE_ENDIAN 1
+#  define B_BIG_ENDIAN    0
+# elif defined(__BIG_ENDIAN__)
+#  define B_LITTLE_ENDIAN 0
+#  define B_BIG_ENDIAN    1
+# endif
+# include <libkern/OSByteOrder.h>
+# define B_SWAP64(x) OSSwapConstInt64(x)
+# define B_SWAP32(x) OSSwapConstInt32(x)
+# define B_SWAP16(x) OSSwapConstInt16(x)
+#elif defined(WINDOWS)
+# define B_LITTLE_ENDIAN 1
+# define B_BIG_ENDIAN    0
+# if defined(NOASM)
+	// C inline version
+# elif defined(_MSC_VER)
+	// inline asm using bswap/xchg
+# elif defined(__GNUC__)
+	// inline asm using bswap/xchg
+# elif defined(__WATCOMC__)
+	// inline asm using bswap/xchg
+# endif
+#endif
+#if !defined(B_LITTLE_ENDIAN) || !defined(B_BIG_ENDIAN)
+# error Unknown endianness
+#endif
+
+#if B_LITTLE_ENDIAN == 1
+# define B_LITTLE64(x) (x)
+# define B_BIG64(x)    B_SWAP64(x)
+# define B_LITTLE32(x) (x)
+# define B_BIG32(x)    B_SWAP32(x)
+# define B_LITTLE16(x) (x)
+# define B_BIG16(x)    B_SWAP16(x)
+#elif B_BIG_ENDIAN == 1
+# define B_LITTLE64(x) B_SWAP64(x)
+# define B_BIG64(x)    (x)
+# define B_LITTLE32(x) B_SWAP32(x)
+# define B_BIG32(x)    (x)
+# define B_LITTLE16(x) B_SWAP16(x)
+# define B_BIG16(x)    (x)
 #endif
 
 #ifndef FP_OFF
-#define FP_OFF(__p) ((unsigned)(__p))
+# define FP_OFF(__p) ((unsigned)(__p))
 #endif
 
 #ifdef __compat_h_macrodef__
 
-#ifndef O_BINARY
-#define O_BINARY 0
-#endif
+# ifndef O_BINARY
+#  define O_BINARY 0
+# endif
 
-#define BO_BINARY O_BINARY
-#define BO_TEXT   O_TEXT
-#define	BO_RDONLY O_RDONLY
-#define BO_WRONLY O_WRONLY
-#define BO_RDWR   O_RDWR
-#define	BO_APPEND O_APPEND
-#define	BO_CREAT  O_CREAT
-#define BO_TRUNC  O_TRUNC
-#define BS_IRGRP  S_IRGRP
-#define BS_IWGRP  S_IWGRP
-#define	BS_IEXEC  S_IEXEC
-#define	BS_IWRITE S_IWRITE
-#define	BS_IREAD  S_IREAD
-#define	BS_IFIFO  S_IFIFO
-#define	BS_IFCHR  S_IFCHR
-#define	BS_IFBLK  S_IFBLK
-#define	BS_IFDIR  S_IFDIR
-#define	BS_IFREG  S_IFREG
-#define BSEEK_SET SEEK_SET
-#define BSEEK_CUR SEEK_CUR
-#define BSEEK_END SEEK_END
+# define BO_BINARY O_BINARY
+# define BO_TEXT   O_TEXT
+# define BO_RDONLY O_RDONLY
+# define BO_WRONLY O_WRONLY
+# define BO_RDWR   O_RDWR
+# define BO_APPEND O_APPEND
+# define BO_CREAT  O_CREAT
+# define BO_TRUNC  O_TRUNC
+# define BS_IRGRP  S_IRGRP
+# define BS_IWGRP  S_IWGRP
+# define BS_IEXEC  S_IEXEC
+# define BS_IWRITE S_IWRITE
+# define BS_IREAD  S_IREAD
+# define BS_IFIFO  S_IFIFO
+# define BS_IFCHR  S_IFCHR
+# define BS_IFBLK  S_IFBLK
+# define BS_IFDIR  S_IFDIR
+# define BS_IFREG  S_IFREG
+# define BSEEK_SET SEEK_SET
+# define BSEEK_CUR SEEK_CUR
+# define BSEEK_END SEEK_END
 #else
-#define BO_BINARY 0
-#define BO_TEXT   1
-#define	BO_RDONLY 2
-#define BO_WRONLY 4
-#define BO_RDWR   6
-#define	BO_APPEND 8
-#define	BO_CREAT  16
-#define BO_TRUNC  32
-#define BS_IRGRP  0
-#define BS_IWGRP  0
-#define	BS_IEXEC  1
-#define	BS_IWRITE 2
-#define	BS_IREAD  4
-#define	BS_IFIFO  0x1000
-#define	BS_IFCHR  0x2000
-#define	BS_IFBLK  0x3000
-#define	BS_IFDIR  0x4000
-#define	BS_IFREG  0x8000
-#define BSEEK_SET 0
-#define BSEEK_CUR 1
-#define BSEEK_END 2
+# define BO_BINARY 0
+# define BO_TEXT   1
+# define BO_RDONLY 2
+# define BO_WRONLY 4
+# define BO_RDWR   6
+# define BO_APPEND 8
+# define BO_CREAT  16
+# define BO_TRUNC  32
+# define BS_IRGRP  0
+# define BS_IWGRP  0
+# define BS_IEXEC  1
+# define BS_IWRITE 2
+# define BS_IREAD  4
+# define BS_IFIFO  0x1000
+# define BS_IFCHR  0x2000
+# define BS_IFBLK  0x3000
+# define BS_IFDIR  0x4000
+# define BS_IFREG  0x8000
+# define BSEEK_SET 0
+# define BSEEK_CUR 1
+# define BSEEK_END 2
 #endif
 
 #ifdef UNDERSCORES
-#define ASMSYM(x) "_" x
+# define ASMSYM(x) "_" x
 #else
-#define ASMSYM(x) x
+# define ASMSYM(x) x
 #endif
 
 #ifndef min
-#define min(a,b) ( ((a) < (b)) ? (a) : (b) )
+# define min(a,b) ( ((a) < (b)) ? (a) : (b) )
 #endif
 
 #ifndef max
-#define max(a,b) ( ((a) > (b)) ? (a) : (b) )
+# define max(a,b) ( ((a) > (b)) ? (a) : (b) )
 #endif
 
-
-#if defined(__WATCOMC__)
-
-#define inline __inline
-#define int64 __int64
-#define longlong(x) x##i64
-
-#elif defined(_MSC_VER)
-
-#define inline __inline
-#define int64 __int64
-#define longlong(x) x##i64
-
-#else
-
-#define longlong(x) x##ll
-typedef long long int64;
-
-#endif
-
-#ifndef NULL
-#define NULL ((void *)0)
-#endif
 
 #define BMAX_PATH 260
 
@@ -136,9 +200,9 @@ int		Bclosedir(BDIR *dir);
 
 
 #ifdef __compat_h_macrodef__
-#define BFILE FILE
-#define bsize_t size_t
-#define bssize_t ssize_t
+# define BFILE FILE
+# define bsize_t size_t
+# define bssize_t ssize_t
 #else
 typedef void          BFILE;
 typedef unsigned long bsize_t;
@@ -147,79 +211,79 @@ typedef signed   long bssize_t;
 
 
 #ifdef __compat_h_macrodef__
-#define Brand rand
-#define Balloca alloca
-#define Bmalloc malloc
-#define Bcalloc calloc
-#define Brealloc realloc
-#define Bfree free
-#define Bopen open
-#define Bclose close
-#define Bwrite write
-#define Bread read
-#define Blseek lseek
-#if defined(__GNUC__)
-#define Btell(h) lseek(h,0,SEEK_CUR)
-#else
-#define Btell tell
-#endif
-#define Bfopen fopen
-#define Bfclose fclose
-#define Bfeof feof
-#define Bfgetc fgetc
-#define Brewind rewind
-#define Bfgets fgets
-#define Bfputc fputc
-#define Bfputs fputs
-#define Bfread fread
-#define Bfwrite fwrite
-#define Bfprintf fprintf
-#define Bstrdup strdup
-#define Bstrcpy strcpy
-#define Bstrncpy strncpy
-#define Bstrcmp strcmp
-#define Bstrncmp strncmp
-#if defined(__WATCOMC__) || defined(_MSC_VER)
-#define Bstrcasecmp stricmp
-#define Bstrncasecmp strnicmp
-#else
-#define Bstrcasecmp strcasecmp
-#define Bstrncasecmp strncasecmp
-#endif
-#if defined(WINDOWS)
-#define Bstrlwr strlwr
-#define Bstrupr strupr
-#endif
-#define Bstrcat strcat
-#define Bstrncat strncat
-#define Bstrlen strlen
-#define Bstrchr strchr
-#define Bstrrchr strrchr
-#define Batoi atoi
-#define Batol atol
-#define Bstrtol strtol
-#define Bstrtoul strtoul
-#define Bstrtod strtod
-#define Btoupper toupper
-#define Btolower tolower
-#define Bmemcpy memcpy
-#define Bmemmove memmove
-#define Bmemchr memchr
-#define Bmemset memset
-#define Bmemcmp memcmp
-#define Bprintf printf
-#define Bsprintf sprintf
-#ifdef _MSC_VER
-#define Bsnprintf _snprintf
-#define Bvsnprintf _vsnprintf
-#else
-#define Bsnprintf snprintf
-#define Bvsnprintf vsnprintf
-#endif
-#define Bvfprintf vfprintf
-#define Bgetcwd getcwd
-#define Bgetenv getenv
-#define Btime() time(NULL)
+# define Brand rand
+# define Balloca alloca
+# define Bmalloc malloc
+# define Bcalloc calloc
+# define Brealloc realloc
+# define Bfree free
+# define Bopen open
+# define Bclose close
+# define Bwrite write
+# define Bread read
+# define Blseek lseek
+# if defined(__GNUC__)
+#  define Btell(h) lseek(h,0,SEEK_CUR)
+# else
+#  define Btell tell
+# endif
+# define Bfopen fopen
+# define Bfclose fclose
+# define Bfeof feof
+# define Bfgetc fgetc
+# define Brewind rewind
+# define Bfgets fgets
+# define Bfputc fputc
+# define Bfputs fputs
+# define Bfread fread
+# define Bfwrite fwrite
+# define Bfprintf fprintf
+# define Bstrdup strdup
+# define Bstrcpy strcpy
+# define Bstrncpy strncpy
+# define Bstrcmp strcmp
+# define Bstrncmp strncmp
+# if defined(__WATCOMC__) || defined(_MSC_VER)
+#  define Bstrcasecmp stricmp
+#  define Bstrncasecmp strnicmp
+# else
+#  define Bstrcasecmp strcasecmp
+#  define Bstrncasecmp strncasecmp
+# endif
+# if defined(WINDOWS)
+#  define Bstrlwr strlwr
+#  define Bstrupr strupr
+# endif
+# define Bstrcat strcat
+# define Bstrncat strncat
+# define Bstrlen strlen
+# define Bstrchr strchr
+# define Bstrrchr strrchr
+# define Batoi atoi
+# define Batol atol
+# define Bstrtol strtol
+# define Bstrtoul strtoul
+# define Bstrtod strtod
+# define Btoupper toupper
+# define Btolower tolower
+# define Bmemcpy memcpy
+# define Bmemmove memmove
+# define Bmemchr memchr
+# define Bmemset memset
+# define Bmemcmp memcmp
+# define Bprintf printf
+# define Bsprintf sprintf
+# ifdef _MSC_VER
+#  define Bsnprintf _snprintf
+#  define Bvsnprintf _vsnprintf
+# else
+#  define Bsnprintf snprintf
+#  define Bvsnprintf vsnprintf
+# endif
+# define Bvfprintf vfprintf
+# define Bgetcwd getcwd
+# define Bgetenv getenv
+# define Btime() time(NULL)
 
 #else
 
