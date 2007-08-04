6a2752b689a018ca0742efe95c08fbb240fa321b
Jonathon Fowler <jf@jonof.id.au>
2007-08-04 13:37:43 +0000

    Game startup window fixed up too

--- testgame/StartupWinController.game.m
+++ testgame/StartupWinController.game.m
@@ -1,7 +1,7 @@
 #import <Cocoa/Cocoa.h>
-#include <stdlib.h>
 
 #include "baselayer.h"
+#include "build.h"
 
 static struct {
 	int fullscreen;
@@ -9,28 +9,22 @@ static struct {
 	int forcesetup;
 } settings;
 
-@interface VideoModeFormatter : NSFormatter
-@end
-
-@implementation VideoModeFormatter
-- (NSString *)stringForObjectValue:(id)object
-{
-	return [NSString stringWithFormat:@"%ld x %ld %ldbpp",
-		object.xdim, object.ydim, object.bpp];
-}
-@end
-
 @interface StartupWinController : NSWindowController
 {
+	NSMutableArray *modeslist3d;
+
 	IBOutlet NSButton *alwaysShowButton;
 	IBOutlet NSButton *fullscreenButton;
 	IBOutlet NSTextView *messagesView;
 	IBOutlet NSTabView *tabView;
-	IBOutlet NSComboBox *videoModeCbox;
+	IBOutlet NSPopUpButton *videoMode3DPUButton;
 	
 	IBOutlet NSButton *cancelButton;
 	IBOutlet NSButton *startButton;
 }
+
+- (void)dealloc;
+- (void)populateVideoModes:(BOOL)firstTime;
 
 - (IBAction)alwaysShowClicked:(id)sender;
 - (IBAction)fullscreenClicked:(id)sender;
@@ -39,90 +33,113 @@ static struct {
 - (IBAction)start:(id)sender;
 
 - (void)setupRunMode;
-- (void)populateForm;
 - (void)setupMessagesMode;
 - (void)putsMessage:(NSString *)str;
 - (void)setTitle:(NSString *)str;
-
-// NSComboBoxDataSource informal protocol
-- (id)comboBox:(NSComboBox *)comboBox objectValueForItemAtIndex:(int)index;
-- (int)numberOfItemsInComboBox:(NSComboBox *)comboBox;
 @end
 
 @implementation StartupWinController
 
+- (void)dealloc
+{
+	[modeslist3d release];
+	[super dealloc];
+}
+
+- (void)populateVideoModes:(BOOL)firstTime
+{
+	int i, mode3d, fullscreen = ([fullscreenButton state] == NSOnState);
+	int idx3d = -1;
+	int xdim, ydim, bpp;
+	
+	if (firstTime) {
+		xdim = settings.xdim3d;
+		ydim = settings.ydim3d;
+		bpp  = settings.bpp3d;
+	} else {
+		mode3d = [[modeslist3d objectAtIndex:[videoMode3DPUButton indexOfSelectedItem]] intValue];
+		if (mode3d >= 0) {
+			xdim = validmode[mode3d].xdim;
+			ydim = validmode[mode3d].ydim;
+			bpp = validmode[mode3d].bpp;
+		}
+		
+	}
+	mode3d = checkvideomode(&xdim, &ydim, bpp, fullscreen, 1);
+	if (mode3d < 0) {
+		int i, cd[] = { 32, 24, 16, 15, 8, 0 };
+		for (i=0; cd[i]; ) { if (cd[i] >= bpp) i++; else break; }
+		for ( ; cd[i]; i++) {
+			mode3d = checkvideomode(&xdim, &ydim, cd[i], fullscreen, 1);
+			if (mode3d < 0) continue;
+			break;
+		}
+	}
+	
+	[modeslist3d release];
+	[videoMode3DPUButton removeAllItems];
+
+	modeslist3d = [[NSMutableArray alloc] init];
+
+	for (i = 0; i < validmodecnt; i++) {
+		if (fullscreen == validmode[i].fs) {
+			if (i == mode3d) idx3d = [modeslist3d count];
+			[modeslist3d addObject:[NSNumber numberWithInt:i]];
+			[videoMode3DPUButton addItemWithTitle:[NSString stringWithFormat:@"%d %C %d %d-bpp",
+				validmode[i].xdim, 0xd7, validmode[i].ydim, validmode[i].bpp]];
+		}
+	}
+
+	if (idx3d >= 0) [videoMode3DPUButton selectItemAtIndex:idx3d];
+}
+
 - (IBAction)alwaysShowClicked:(id)sender
 {
-	settings.forcesetup = [sender state] == NSOnState;
 }
 
 - (IBAction)fullscreenClicked:(id)sender
 {
-	settings.fullscreen = [sender state] == NSOnState;
-	// XXX: recalculate the video modes list to take into account the fullscreen status
+	[self populateVideoModes:NO];
 }
 
 - (IBAction)cancel:(id)sender
 {
 	[NSApp abortModal];
-	[self clearVideoModeMap];
 }
 
 - (IBAction)start:(id)sender
 {
+	int mode = [[modeslist3d objectAtIndex:[videoMode3DPUButton indexOfSelectedItem]] intValue];
+	if (mode >= 0) {
+		settings.xdim3d = validmode[mode].xdim;
+		settings.ydim3d = validmode[mode].ydim;
+		settings.bpp3d = validmode[mode].bpp;
+		settings.fullscreen = validmode[mode].fs;
+	}
+		
+	settings.forcesetup = [alwaysShowButton state] == NSOnState;
+
 	[NSApp stopModal];
 }
 
 - (void)setupRunMode
 {
-	// XXX: populate the lists and set everything up to represent the current options
-	[self populateForm];
+	getvalidmodes();
+
+	[fullscreenButton setState: (settings.fullscreen ? NSOnState : NSOffState)];
+	[alwaysShowButton setState: (settings.forcesetup ? NSOnState : NSOffState)];
+	[self populateVideoModes:YES];
 
 	// enable all the controls on the Configuration page
 	NSEnumerator *enumerator = [[[[tabView tabViewItemAtIndex:0] view] subviews] objectEnumerator];
 	NSControl *control;
-	while (control = [enumerator nextObject])
-		[control setEnabled:true];
+	while (control = [enumerator nextObject]) [control setEnabled:true];
 	
 	[cancelButton setEnabled:true];
 	[startButton setEnabled:true];
 
 	[tabView selectTabViewItemAtIndex:0];
-}
-
-- (void)populateForm
-{
-	int mode3d, i;
-	
-	mode3d = checkvideomode(&settings.xdim3d, &settings.ydim3d, settings.bpp3d, settings.fullscreen, 1);
-	if (mode3d < 0) {
-		int i, cd[] = { 32, 24, 16, 15, 8, 0 };
-		for (i=0; cd[i]; ) { if (cd[i] >= settings.bpp3d) i++; else break; }
-		for ( ; cd[i]; i++) {
-			mode3d = checkvideomode(&settings.xdim3d, &settings.ydim3d, cd[i], settings.fullscreen, 1);
-			if (mode3d < 0) continue;
-			settings.bpp3d = cd[i];
-			break;
-		}
-	}
-	
-	[fullscreenButton setState:(settings.fullscreen ? NSOnState : NSOffState)];
-	[alwaysShowButton setState:(settings.forcesetup ? NSOnState : NSOffState)];
-	
-	[self clearVideoModeMap];
-	for (i=0; i<validmodecnt; i++) {
-		if (validmode[i].fs != settings.fullscreen) continue;
-		
-		NSString *buf = [[NSString alloc] initWithFormat:
-					@"%ld x %ld %ldbpp",
-					validmode[i].xdim,
-					validmode[i].ydim,
-					validmode[i].bpp];
-	}
-
-	[videoModeCbox removeAllItems];
-	[videoModeCbox setFormatter:[[VideoModeFormatter alloc] init]];
-	[videoModeCbox addItemsWithObjectValues:[NSArray arrayWithObjects:validmode count:validmodecnt]];
+	[NSCursor unhide];	// Why should I need to do this?
 }
 
 - (void)setupMessagesMode
@@ -137,8 +154,7 @@ static struct {
 		if (control == alwaysShowButton) continue;
 		[control setEnabled:false];
 	}
-	
-	[enumerator release];
+
 	[cancelButton setEnabled:false];
 	[startButton setEnabled:false];
 }
@@ -170,21 +186,7 @@ static struct {
 	[[self window] setTitle:str];
 }
 
-- (int)numberOfItemsInComboBox:(NSComboBox *)comboBox
-{
-	if (comboBox == videoModeCbox) {
-	}
-	return 0;
-}
-
-- (id)comboBox:(NSComboBox *)comboBox objectValueForItemAtIndex:(int)value
-{
-	if (comboBox == videoModeCbox) {
-	}
-	return 0;
-}
-
-@end	// implementation StartupWinController
+@end
 
 static StartupWinController *startwin = nil;
 
@@ -195,11 +197,8 @@ int startwin_open(void)
 	startwin = [[StartupWinController alloc] initWithWindowNibName:@"startwin.game"];
 	if (startwin == nil) return -1;
 
-	[startwin showWindow:nil];
 	[startwin setupMessagesMode];
-	
-	// so that SDL's chicanery doesn't interfere with our UI
-	setenv("SDL_ENABLEAPPEVENTS","1",1);
+	[startwin showWindow:nil];
 
 	return 0;
 }
@@ -209,9 +208,8 @@ int startwin_close(void)
 	if (startwin == nil) return 1;
 
 	[startwin close];
+	[startwin release];
 	startwin = nil;
-	
-	unsetenv("SDL_ENABLEAPPEVENTS");
 
 	return 0;
 }
@@ -271,16 +269,16 @@ int startwin_run(void)
 		case NSRunAbortedResponse: retval = 0; break;
 		default: retval = -1;
 	}
+	
+	[startwin setupMessagesMode];
 
-	if (retval > 0) {
+	if (retval) {
 		fullscreen = settings.fullscreen;
 		xdimgame = settings.xdim3d;
 		ydimgame = settings.ydim3d;
 		bppgame = settings.bpp3d;
 		forcesetup = settings.forcesetup;
 	}
-	
-	[startwin setupMessagesMode];
 	
 	return retval;
 }
