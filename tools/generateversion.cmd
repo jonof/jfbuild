cf11b56c1903c2cf529758f4954be134a3a3c317
Jonathon Fowler <jf@jonof.id.au>
2023-07-09 12:51:28 +1000

    win: fix version-auto.c when building from a non-git source tree
    fixes #22

--- tools/generateversion.cmd
+++ tools/generateversion.cmd
@@ -1,11 +1,11 @@
 @ECHO OFF
 REM Generator for version-auto.c on Windows
 
-git.exe --version >NUL 2>&1
+git.exe rev-parse HEAD >NUL 2>&1
 IF %ERRORLEVEL% EQU 0 (
     FOR /F %%G IN ('git.exe describe --always') DO ECHO const char *build_version = "%%G";
 ) ELSE (
-    ECHO const char *build_version = "git error";
+    ECHO const char *build_version = "not available";
 )
 ECHO const char *build_date = __DATE__;
 ECHO const char *build_time = __TIME__;
